26be8c56447947397b5f0edb10136268c62fac0f
Peter Li <petercyli02@gmail.com>
2024-06-17 15:32:31 +0100

    First commit

---
+++ backend/todo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ToDoSerializer
+from .models import ToDo
+
+# Create your views here.
+
+class ToDoView(viewsets.ModelViewSet):
+    serializer_class = ToDoSerializer
+    queryset = ToDo.objects.all()
